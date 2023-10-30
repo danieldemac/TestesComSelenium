9fa07d2541d023438c9c75bcca3455c793b84940
Daniel Cabral <79099414+danieldemac@users.noreply.github.com>
2023-10-30 14:00:25 -0300

    Add files via upload

--- SeleniumTestes/cep_teste.py
+++ SeleniumTestes/cep_teste.py
@@ -1,3 +1,4 @@
+from fpdf import FPDF
 import time
 import pandas as pd
 from selenium import webdriver
@@ -10,6 +11,9 @@ def selecionar_arquivo():
     caminho_arquivo_excel = filedialog.askopenfilename()
     arquivo_selecionado.set(caminho_arquivo_excel)
     selecionar_arquivo_button.config(bg="green")
+    imprimir_resultados_button.config(bg="red")
+
+resultados = []  # Variável global para armazenar os resultados
 
 def buscar_ceps():
     if not caminho_arquivo_excel:
@@ -35,28 +39,50 @@ def buscar_ceps():
         # Agora, você deve pegar o elemento correto que contém o intervalo de CEPs
         cep_element = driver.find_element(By.XPATH, "//table[@class='tmptabela']//tr[3]/td[2]")
         cep = cep_element.text
+        resultados.append(f"UF: {uf}, Localidade: {localidade}, CEP: {cep}")
+        resultados.append("--------------------------")
+
         resultado_text.insert('end', f"UF: {uf}, Localidade: {localidade}, CEP: {cep}\n")
+        resultado_text.insert('end', "--------------------------\n")
 
         # Voltar para a página de busca
         driver.get("https://www2.correios.com.br/sistemas/buscacep/buscaFaixaCEP.cfm")
 
     driver.quit()
+    imprimir_resultados_button.config(bg="blue")
+
+def imprimir_resultados():
+    if resultados:
+        nome_arquivo = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=[("PDF files", "*.pdf")])
+        if nome_arquivo:
+            pdf = FPDF()
+            pdf.add_page()
+            pdf.set_font("Arial", size=12)
+
+            for resultado in resultados:
+                pdf.multi_cell(0, 10, resultado)
+                pdf.ln(10)
+
+            pdf.output(nome_arquivo)
 
 root = tk.Tk()
 root.title("Buscar CEPs v.Beta - By Daniel Cabral")
 
 selecionar_arquivo_button = Button(root, text="Selecionar Arquivo Excel", command=selecionar_arquivo, bg="red")
-selecionar_arquivo_button.pack()
+selecionar_arquivo_button.grid(row=0, column=0)
 
 arquivo_selecionado = tk.StringVar()
 arquivo_selecionado_label = tk.Label(root, textvariable=arquivo_selecionado, fg="green")
-arquivo_selecionado_label.pack()
+arquivo_selecionado_label.grid(row=0, column=1)
 
 buscar_ceps_button = Button(root, text="Buscar CEPs", command=buscar_ceps, bg="green")
-buscar_ceps_button.pack()
+buscar_ceps_button.grid(row=0, column=2)
+
+imprimir_resultados_button = Button(root, text="Imprimir Resultados", command=imprimir_resultados, bg="red")
+imprimir_resultados_button.grid(row=0, column=3)
 
 resultado_text = Text(root)
-resultado_text.pack()
+resultado_text.grid(row=1, columnspan=4)
 
 caminho_arquivo_excel = ""
 
